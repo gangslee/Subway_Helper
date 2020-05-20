e3f80b54be10ad4d25a604aac29610c302e3ec01
NongE <45160294+NongE@users.noreply.github.com>
2020-05-21 01:25:29 +0900

    안드로이드-DB 연결 수정 / UI 수정 / 기타 수정
    안드로이드-DB 연결 수정
    - 사용자의 요청에 따라 값을 보내고 받아 올 수 있도록 수정 (POST)
    UI 수정
    - 조회 결과 창을 보여주는 askActivity의 디자인 수정
    기타 수정
    - 서버로부터 데이터를 받아 올때 line정보를 int에서 String 으로 변경
    - 경고 구문 수정 (private, val 설정 등)

--- SubwayHelper/app/src/main/java/com/example/subwayhelper/server/ServerAPI.kt
+++ SubwayHelper/app/src/main/java/com/example/subwayhelper/server/ServerAPI.kt
@@ -2,6 +2,7 @@ package com.example.subwayhelper.server
 
 import com.example.subwayhelper.data.LatestData
 import com.example.subwayhelper.data.UserResponse
+import com.example.subwayhelper.data.getData
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -10,8 +11,8 @@ import retrofit2.http.POST
 
 
 interface ServiceApi {
-    @GET("/subway")
-    fun getData(): Call<UserResponse?>?
+    @POST("/subway")
+    fun getData(@Body data: getData?): Call<UserResponse?>?
     /*
     @POST("/user/join")
     fun joinUser(@Body data: LatestData?): Call<UserResponse?>?
